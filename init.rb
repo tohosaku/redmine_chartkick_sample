2c71bdd8b51c65aa9da92981e77c836108c851a9
Takashi Kato <tohosaku@users.osdn.me>
2023-10-05 06:49:10 +0900

    Initial Commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_chartkick_sample do
+  name 'Redmine Chartkick Sample plugin'
+  author 'tohosaku'
+  description 'sample plugin with chartkick gem'
+  version '0.0.1'
+  url 'https://github.com/tohosaku/redmine_chartkick_sample'
+  author_url 'https://github.com/tohosaku'
+end
